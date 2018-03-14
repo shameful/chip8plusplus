8ef191b50c7395c79223e50a09f4902a948e5686
shameful <shameful@users.noreply.github.com>
2018-03-14 16:30:52 -0500

    move memory definition to header and change access to protected

--- chip8++/Memory.cpp
+++ chip8++/Memory.cpp
@@ -5,11 +5,6 @@ namespace Mem
 {
 	const std::uint8_t BIT_PATTERN = 0xAA;
 
-	std::array<std::uint8_t, 0x1000> memory;
-	//mapping
-	//0x000 - 0x1FF reserved for interpreter
-	//0x200 - 0xFFF(?) program space
-
 	std::uint8_t Chip8Mem::read(uint16_t addr)
 	{
 		std::uint8_t data = BIT_PATTERN;
--- chip8++/Memory.h
+++ chip8++/Memory.h
@@ -4,7 +4,14 @@
 namespace Mem
 {
 	
-	class Chip8Mem {
+	class Chip8Mem
+	{
+
+	protected:
+		std::array<std::uint8_t, 0x1000> memory;
+		//mapping
+		//0x000 - 0x1FF reserved for interpreter
+		//0x200 - 0xFFF(?) program space
 
 	public:
 		std::uint8_t read(uint16_t addr);
